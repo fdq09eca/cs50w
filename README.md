473918ec006c3c8c0b97e1fff8906e3c7883b588
Thanh Bui <macone@Macs-MacBook-Pro.local>
2020-05-26 13:41:47 +0800

    readme

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
-# Project 1
+# CS50w Project 1
 
-Web Programming with Python and JavaScript
+## Overview
+
+It is a book searching website.
+
+- In particular, after login, it allows users to search, rate and, comment for a book which exists in the database.
+- Users can see their own rating of a book and its average rating which is rated by other users
+- Moreover, users' book comment will be displayed to the public.
+- Lastly, it opens an public API for querying a book's detail, average rating and number of rating by ISBN.
+
+## Demo
+
+Youtube: 
+Heroku: [https://cs50w-pset1-book.herokuapp.com/](https://cs50w-pset1-book.herokuapp.com/)
+
+## Features
+
+This project has the following features:
+
+1. GUI Database interaction
+   1. login, resigter system
+   2. Searching GUI
+   3. Comment and rating
+2. Public API
+
+## Technologies
+
+- Flask
+- PostgradSQL
+- Bootstrap
+- Sass
+- HTML
+
+## Requirement
+
+1. Registration
+2. Login
+3. Logout
+4. Import
+5. Search
+6. Book Page
+7. Goodreads Review Data
+8. API Access
--- import.py
+++ import.py
@@ -2,32 +2,18 @@ import os
 import csv
 from application import db
 import requests
-api_key = os.getenv('API_KEY')
-
-
-def api_res(isbn):
-    res = requests.get("https://www.goodreads.com/book/review_counts.json",
-                       params={"key": api_key, "isbns": isbn})
-    if res.status_code == 200:
-        bk = res.json()['books'][0]
-        return bk.get('work_ratings_count'), bk.get('average_rating')
-
-
 file = open("books.csv")
 
 reader = csv.reader(file)
-next(reader)
+next(reader)  # skip the header
 for isbn, title, author, year in reader:
-    review_count, average_score = api_res(isbn)
 
     db.execute("INSERT INTO books (isbn, title, author, year, review_count, average_score) VALUES (:isbn, :title, :author, :year, :review_count, :average_score)",
                {"isbn": isbn,
                 "title": title,
                 "author": author,
-                "year": year,
-                "review_count": review_count,
-                "average_score": average_score})
-    # print(isbn, title, author, year)
+                "year": year})
+
     print(f"Added book {title} to database.")
 
     db.commit()
